e37876cbaab80cb306823a57621ecd31e07b5ed4
Sherman Au <shermau13@gmail.com>
2022-03-05 14:44:47 -0700

    feat:NightWriter #reads/writes from message.txt to braille.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,8 @@
-p "Created 'message.txt' containing 256 characters"
+english_reader = File.open(ARGV[0], 'r')
+english_message = english_reader.read
+english_reader.close
+
+braille_message = english_message.downcase
+braille_writer = File.open(ARGV[1], 'w')
+braille_writer.write(braille_message)
+puts "Created 'braille.txt' containing #{braille_message.length} characters"
