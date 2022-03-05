111fdce2f43b5d544832a2b2483ca32e525c6e56
Sherman Au <shermau13@gmail.com>
2022-03-05 14:11:06 -0700

    test:#writer_spec and spec_helper

---
+++ spec/night_writer_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/night_writer'
+require 'pry'
+
+RSpec.describe do
+  context 'Iteration 1' do
+    describe NightWriter do
+      before(:each) do
+
+      end
+
+      it 'exists' do
+
+      end
+
+      it 'has attributes' do
+
+      end
+    end
