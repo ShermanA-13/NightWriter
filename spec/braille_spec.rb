fc27308b0a223e1e59844c0c4c6f818f3e7c07c1
Sherman Au <shermau13@gmail.com>
2022-03-06 13:57:34 -0700

    test:#BrailleTranslator exists

---
+++ spec/braille_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../lib/braille_alphabet'
+require_relative '../lib/braille_translator'
+require 'pry'
+
+RSpec.describe do
+  context 'Iteration 2' do
+    describe BrailleAlphabet do
+      before(:each) do
+        @br_alpha = BrailleAlphabet.new
+      end
+
+      it 'exists' do
+        expect(@br_alpha).to be_a(BrailleAlphabet)
+      end
+
+      it 'has attributes' do
+        expect(@br_alpha.braille_letters).to be_a(Hash)
+        expect(@br_alpha.braille_letters[:a]).to eq(['0.', '..', '..'])
+        expect(@br_alpha.braille_letters[:" "]).to eq(['..', '..', '..'])
+      end
+    end
+
+    describe BrailleTranslator do
+      context 'Iteration 2' do
+        describe BrailleTranslator do
+          before(:each) do
+            @br_translator = BrailleTranslator.new
+          end
+
+          it 'exists' do
+            expect(@br_translator).to be_a(BrailleTranslator)
+          end
+        end
+      end
+    end
+  end
+end
