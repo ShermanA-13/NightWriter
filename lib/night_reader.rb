2b0f93d3b539e16d12a0e1f686b59d21bd81e6a2
Sherman Au <shermau13@gmail.com>
2022-03-09 09:29:52 -0700

    refactor: night_reader syntax

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -2,10 +2,10 @@ require_relative '../lib/english_translator'
 
 braille_reader = File.open(ARGV[0], 'r')
 braille_message = braille_reader.read.chomp
-braille_writer.reader.close
+braille_reader.close
 
-# eng_translator = EnglishTranslator.new
-# english_message = english.read.chomp
+eng_translator = EnglishTranslator.new
+english_message = eng_translator.translate(braille_message.downcase)
 english_writer = File.open(ARGV[1], 'w')
 english_writer.write(english_message)
 
